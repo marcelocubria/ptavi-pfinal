8e3d3baf01f6309853208dfd051514ef2a2ed204
Marcelo Cubria Alonso <mcubria@gamma26.aulas.gsyc.urjc.es>
2018-12-05 10:27:44 +0100

    terminado ua1.xml correctamente, lectura de xml en cliente e inicio del registro

--- uaclient.py
+++ uaclient.py
@@ -6,6 +6,7 @@ Programa cliente que abre un socket a un servidor
 
 import socket
 import sys
+from xml.dom import minidom
 
 # Cliente UDP simple.
 
@@ -20,10 +21,26 @@ except (IndexError, ValueError):
 # Contenido que vamos a enviar
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
+    archivo_xml = minidom.parse(config)
+    #obtengo la informacion de mi usuario y servidor
+    account = archivo_xml.getElementsByTagName('account')
+    uaserver= archivo_xml.getElementsByTagName('uaserver')
+    mi_usuario = account[0].attributes['username'].value
+    mi_server = uaserver[0].attributes['ip'].value
+    if mi_server == '':
+        mi_server = "127.0.0.1"
+    mi_puerto = uaserver[0].attributes['puerto'].value
+    
+    #obtengo informacion del servidor regproxy y me conecto
+    regproxy = archivo_xml.getElementsByTagName('regproxy')
+    IP_regproxy = regproxy[0].attributes['ip'].value
+    puerto_regproxy = int(regproxy[0].attributes['puerto'].value)
+    
     my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    my_socket.connect((IP, PORT))
+    my_socket.connect((IP_regproxy, puerto_regproxy))
 
-    linea = (PETICION + " sip:" + RECEPTOR + "@" + IP + " SIP/2.0\r\n")
+    linea = (METODO + " sip:" + mi_usuario + "@" + mi_server +
+             ":" + mi_puerto + " SIP/2.0\r\n" + "Expires: " + OPCION + "\r\n")
 
     print("Enviando: " + linea)
     my_socket.send(bytes(linea, 'utf-8') + b'\r\n')
